dd0020b91dbde51f0c9d05dc06daf10b3074d073
Ambika Dash <ambikaprasanna@efi.com>
2016-09-06 15:23:36 +0530

    initial working commit for uib

--- imx6/soc/imx6dq.mk
+++ imx6/soc/imx6dq.mk
@@ -9,7 +9,7 @@ HAVE_FSL_IMX_GPU3D := true
 HAVE_FSL_IMX_IPU := true
 BOARD_KERNEL_BASE := 0x14000000
 LOAD_KERNEL_ENTRY := 0x10008000
-TARGET_KERNEL_DEFCONF := imx_v7_android_defconfig
+TARGET_KERNEL_DEFCONF := imx_v7_uib_android_defconfig
 -include external/fsl_vpu_omx/codec_env.mk
 -include external/fsl_imx_omx/codec_env.mk
 TARGET_HAVE_IMX_GRALLOC := true
